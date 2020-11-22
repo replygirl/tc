cae42b35eb61b49c2fef87d89d75e6f9706b1c74
replygirl <moon@moon.fyi>
2020-11-22 18:35:18 -0500

    support async handlers

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,17 +4,45 @@ import tc from '.'
 
 const eq = (a: any, b: any) => () => assert.deepStrictEqual(a, b)
 
-describe('tc', () => {
+describe('tc', async () => {
   it(
-    'should return [x] when cb returns a x',
-    eq(tc(() => true), [true])
+    'should return [x] when cb returns x',
+    eq(tc<true>(() => true), [true])
+  )
+  it(
+    'should return Promise<[x]> when async cb returns x',
+    eq(await tc<true, true>(async () => true), [true])
   )
   it(
     'should return [undefined, e] when cb throws w/o fallback',
-    eq(tc(() => { throw new Error() }), [undefined, new Error()] )
+    eq(tc<any>(() => { throw new Error() }), [undefined, new Error()])
   )
   it(
-    'should return [y, e] when cb throws w/ fallback',
-    eq(tc(() => { throw new Error() }, () => true), [true, new Error()])
+    'should return [undefined, e] when async cb throws w/o fallback',
+    eq(
+      await tc<any, true>(async () => { throw new Error() }),
+      [undefined, new Error()]
+    )
+  )
+  it(
+    'should return [fb, e] when cb throws w/ fallback',
+    eq(
+      tc<true>(() => { throw new Error() }, () => true),
+      [true, new Error()]
+    )
+  )
+  it(
+    'should return Promise<[fb, e]> when async cb throws w/ fallback',
+    eq(
+      await tc<true, true>(async () => { throw new Error() }, async () => true),
+      [true, new Error()]
+    )
+  )
+  it(
+    'should return Promise<[fb, e]> when async cb throws w/ async fallback',
+    eq(
+      await tc<true, true>(async () => { throw new Error() }, async () => true),
+      [true, new Error()]
+    )
   )
 })
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@
  * @return {any}
  */
 
- /**
+/**
  * @typedef returnArray
  * @type {array}
  * @property {any} [0] - The value returned by the callback or fallback
  * @property {unknown} [1] - The error thrown by the callback
  */
+export type TcResult<X = unknown, IsAsync extends boolean = false> =
+  IsAsync extends true
+    ? Promise<[X?, unknown?]>
+    : [X?, unknown?]
 
 /**
  * Execute a callback within a `try...catch` statement,
@@ -34,6 +38,15 @@
  *     console.info(y ?? ye) // Error
  *
  *
+ *     // With async functions
+ *
+ *     const [x, xe] = await tc(async () => true)
+ *     console.info(x ?? xe) // true
+ *
+ *     const [y, ye] = await tc(async () => { throw new Error() })
+ *     console.info(y ?? ye) // Error
+ *
+ *
  *     // Error handling
  *
  *     tc(() => { throw new Error() }, e => console.error(e)) // Error
@@ -47,12 +60,26 @@
  *     const [y] = tc(() => { throw new Error() }, e => false)
  *     console.info(y) // false
  */
-export function tc<X = unknown>(
-  cb: () => X,
-  fb?: (e: unknown) => any
-): [X?, unknown?] {
-  try { return [cb()] }
-  catch (e: unknown) { return [fb?.(e), e]}
+export function tc<X = unknown, IsAsync extends boolean = false>(
+  cb: () => IsAsync extends true ? Promise<X> : X,
+  fb?: (e: unknown) => IsAsync extends true ? Promise<X> : X
+): TcResult<X, IsAsync> {
+  try {
+    const x = cb()
+    return x instanceof Promise
+      ? new Promise<[X?, unknown?]>(async resolve => {
+        try { resolve([await x]) }
+        catch (e: unknown) {
+          resolve([
+            await (fb?.(e) as Promise<X | undefined>), e
+          ] as unknown as TcResult<X, IsAsync>)
+        }
+      }) as TcResult<X, IsAsync>
+      : [x] as unknown as TcResult<X, IsAsync>
+  }
+  catch (e: unknown) {
+    return [fb?.(e), e] as unknown as TcResult<X, IsAsync>
+  }
 }
 
 export default tc
