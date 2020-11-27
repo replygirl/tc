1b06a9b6dbf13832b072a2c969f87a6835be4c36
moon <moon@moon.fyi>
2020-11-27 11:00:15 -0500

    docs(readme): improve typescript docs

--- README.md
+++ README.md
@@ -66,7 +66,13 @@ console.info(z) // true
 ## Changes from 1.x
 
 - **Breaking:** The `IsAsync` type argument is no longer required
-- **Breaking:** The `TcResult` type has been simplified to `[T?, unknown?]`
+- **Breaking for some TypeScript cases:** For flexibility between sync & async, `tc<T>` will now return a `TcReturn<T>`, which breaks out like this:
+  ```ts
+  type TcResult<T = unknown> = [MaybePromise<Devoided<T>>, unknown?]
+  type TcReturn<T = unknown> = MaybePromise<TcResult<T>>
+  type MaybePromise<T = unknown> = T | Promise<T>
+  type Devoided<T = unknown> = T | undefined
+  ```
 - Synchronous & async handlers can be mixed & matched
 - Native ES2019 (compiled from ES2021)
 - Zero-install Yarn 2
--- src/tc.types.ts
+++ src/tc.types.ts
@@ -5,14 +5,14 @@ import type { MaybePromise } from './index.types'
  * @callback TcCallback
  * @return {*}
  */
-export type TcCallback<T> = () => MaybePromise<T>
+export type TcCallback<T = unknown> = () => MaybePromise<T>
 
 /**
  * @callback TcErrorHandler
  * @param {unknown} e - An error
  * @return {*}
  */
-export type TcErrorHandler<T> = (
+export type TcErrorHandler<T = unknown> = (
   e: unknown
 ) => MaybePromise<T> | MaybePromise<void>
 
@@ -22,6 +22,9 @@ export type TcErrorHandler<T> = (
  * @property {any} [0] - The value returned by the callback or fallback
  * @property {unknown} [1] - The error thrown by the callback
  */
-export type TcResult<T> = [MaybePromise<Devoided<T>>, unknown?]
+export type TcResult<T = unknown> = [
+  MaybePromise<Devoided<T>>,
+  unknown?
+]
 
-export type TcReturn<T> = MaybePromise<TcResult<T>>
+export type TcReturn<T = unknown> = MaybePromise<TcResult<T>>
