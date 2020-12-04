70a8beef4582dc916482a9b770fa8dc9e41bb5fe
moon <moon@moon.fyi>
2020-12-04 03:38:04 -0500

    text default

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,28 @@
 import { deepStrictEqual as eq } from 'assert'
 
-import tc, { tcs } from '.'
+import _default, { tc, tcs } from '.'
+
+describe('_default', () => {
+  it('when cb returns x, returns Promise<[x]>', async () =>
+    eq(await _default(async () => true), [true]))
+  it('when async cb throws w/o fallback, returns [undefined, e]', async () =>
+    eq(
+      await _default(async () => {
+        throw new Error()
+      }),
+      [undefined, new Error()]
+    ))
+  it('when cb throws w/ fallback, returns Promise<[fb, e]>', async () =>
+    eq(
+      await _default(
+        async () => {
+          throw new Error()
+        },
+        async () => true
+      ),
+      [true, new Error()]
+    ))
+})
 
 describe('tc', () => {
   it('when cb returns x, returns Promise<[x]>', async () =>
