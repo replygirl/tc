32a259958da7c317c09b6f84682cf42c86e88eb7
replygirl <moon@moon.fyi>
2022-03-27 07:23:07 -0400

    fix: improve inference

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,30 @@
 type MaybePromise<T> = Promise<T> | T
 
 function tc<T = unknown, U = T>(
-  t: () => T,
-  c?: (e: unknown) => U
+  t: () => T extends Promise<any> ? never : T
+): [T, undefined] | [undefined, unknown]
+function tc<T = unknown, U = T>(
+  t: () => T extends Promise<any> ? never : T,
+  c: (e: unknown) => U extends Promise<any> ? never : U
 ): [T, undefined] | [U | undefined, unknown]
 function tc<T = unknown, U = T>(
-  t: () => T,
-  c?: (e: unknown) => Promise<U>
+  t: () => T extends Promise<any> ? never : T,
+  c: (e: unknown) => Promise<U extends Promise<any> ? never : U>
 ): [T, undefined] | Promise<[U | undefined, unknown]>
 function tc<T = unknown, U = T>(
-  t: () => Promise<T>,
-  c?: (e: unknown) => MaybePromise<U>
+  t: () => Promise<T extends Promise<any> ? never : T>
+): Promise<[T, undefined] | [undefined, unknown]>
+function tc<T = unknown, U = T>(
+  t: () => Promise<T extends Promise<any> ? never : T>,
+  c: (e: unknown) => U extends Promise<any> ? never : U
 ): Promise<[T, undefined] | [U | undefined, unknown]>
 function tc<T = unknown, U = T>(
-  t: () => MaybePromise<T>,
-  c?: (e: unknown) => MaybePromise<U>
+  t: () => Promise<T extends Promise<any> ? never : T>,
+  c: (e: unknown) => Promise<U extends Promise<any> ? never : U>
+): Promise<[T, undefined] | [U | undefined, unknown]>
+function tc<T = unknown, U = T>(
+  t: () => MaybePromise<T extends Promise<any> ? never : T>,
+  c?: (e: unknown) => MaybePromise<U extends Promise<any> ? never : U>
 ): MaybePromise<[T, undefined] | [U | undefined, unknown]> {
   try {
     const tv = t()
